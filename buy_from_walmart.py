2125b6120a505915345e974fcb1e961ec4fa7ede
Raad Altaie <raad.altaie@icloud.com>
2021-04-18 02:29:47 -0700

    added check price and some refactor

--- buy_from_walmart.py
+++ buy_from_walmart.py
@@ -12,15 +12,16 @@ import time
 
 item_link = 'https://www.walmart.com/ip/Sony-PlayStation-5-Video-Game-Console/363472942'
 
+max_buying_price = 500
 
-first_name = 'Test'
-last_name = 'Test'
-email = 'test@test.com'
-phone = '8008080000'
-address_line_one = '123 Main St'
-city = 'San Diego'
+first_name = 'John'
+last_name = 'Doe'
+email = 'john.doe@example.com'
+phone = '818-379-6722'
+address_line_one = '1600 Amphitheatre Pkwy'
+city = 'Mountain View'
 state = 'California'
-zip_code = '920000'
+zip_code = '94043'
 credit_card_number = '4872475635924104'
 credit_card_exp_month = '05'
 credit_card_exp_year = '27'
@@ -37,48 +38,56 @@ def buy_from_walmart():
 
         if driver.find_elements_by_xpath('//*[(text()="Add to cart")]'):
 
-            add_to_cart = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.CLASS_NAME,'spin-button-children')))
-            add_to_cart.click()
+            # Check the price
+            price_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'#price > div > span.hide-content.display-inline-block-m > span > span.visuallyhidden')))
+            price = float(price_element.text.replace('$', ''))
+
+            if price > float(max_buying_price):
+                print("Item is more expensive than max_buying_price")
+                time.sleep(60)
+                driver.refresh()
+                continue
 
 
-            time.sleep(2)
-            WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Check out")]'))).click()
-            WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Continue without account")]'))).click()
-            WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Continue")]'))).click()
+            # Add to cart
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME,'spin-button-children'))).click()
 
-            time.sleep(2)
+            WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Check out")]'))).click()
+            WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Continue without account")]'))).click()
+            WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Continue")]'))).click()
 
 
-            driver.find_element_by_id("firstName").send_keys(first_name)
-            driver.find_element_by_id("lastName").send_keys(last_name)
-            driver.find_element_by_id("phone").send_keys(phone)
-            driver.find_element_by_id("email").send_keys(email)
-            driver.find_element_by_id("addressLineOne").send_keys(address_line_one)
+            # Fill in the info
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'firstName'))).send_keys(first_name)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'lastName'))).send_keys(last_name)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'phone'))).send_keys(phone)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'email'))).send_keys(email)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'addressLineOne'))).send_keys(address_line_one)
+
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'city'))).clear()
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'city'))).send_keys(city)
+
+            Select(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'state')))).select_by_visible_text(state)
             
-
-            driver.find_element_by_id('city').clear()
-            driver.find_element_by_id("city").send_keys(city)
-
-
-            Select(driver.find_element_by_id("state")).select_by_visible_text(state)
-
-            driver.find_element_by_id("postalCode").clear()
-            driver.find_element_by_id("postalCode").send_keys(zip_code) 
-            
-            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Continue")]'))).click()
-            time.sleep(2)
-
-            driver.find_element_by_id("creditCard").send_keys(credit_card_number)
-            Select(driver.find_element_by_id("month-chooser")).select_by_visible_text(credit_card_exp_month)
-            Select(driver.find_element_by_id("year-chooser")).select_by_visible_text(credit_card_exp_year)
-            driver.find_element_by_id("cvv").send_keys(credit_card_cvv)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'postalCode'))).clear()
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'postalCode'))).send_keys(zip_code)
 
 
+            # Click Continue after enter the info
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Continue")]'))).click()
 
-            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Review your order")]'))).click()
-            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Place order")]'))).click()
+
+            # Payment info
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'creditCard'))).send_keys(credit_card_number)
+            Select(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'month-chooser')))).select_by_visible_text(credit_card_exp_month)
+            Select(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'year-chooser')))).select_by_visible_text(credit_card_exp_year)            
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,'cvv'))).send_keys(credit_card_cvv)
+
+            # Place order
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Review your order")]'))).click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[(text()="Place order")]'))).click()
             got_item = True
-            print('GOT THE ITEM!! YAY!')
+            print('YOU GOT THE ITEM!! YAY!')
 
 
         else:
